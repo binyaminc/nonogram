07b68955c436bfdb1f48c8b3daeda34120f5958e
bin <binyaminc6@gmail.com>
2021-03-03 22:48:39 +0200

    smart choice of row/columns
    using heuristic function, sorts the rows (for example)  by priority, and computes the ITER_AMOUNT best. using flags array, computes only the rows/columns that has a change since the last time checked

--- nonogram.py
+++ nonogram.py
@@ -5,14 +5,16 @@ class state(Enum):
     Black = 1
     Unknown = 0
     White = -1
-    
-    
+   
+REC_COUNTER = [0]
+
+
 # this example creates אייל
 
 ROWS = 15
 COLUMNS = 15
 Matrix = [[state.Unknown for x in range(COLUMNS)] for y in range(ROWS)]
-REC_COUNTER = [0]
+
 
 #array that describes the rows of the matrix, up to down
 values_rows_arr = [[2,2,2,1], [2,2,1,1,2,1], [4,1,1,2,1], [5,5], [2,5],[7], [1,1,5], [11], [12], [13],[8,5], [1,3,5], [5,5], [3,6], [6]]
@@ -20,6 +22,10 @@ values_rows_arr = [[2,2,2,1], [2,2,1,1,2,1], [4,1,1,2,1], [5,5], [2,5],[7], [1,1
 values_columns_arr = [[2], [3,3], [2,2,2], [4,6], [5,7],[2,7], [3,1,4], [7], [2,4], [2,6,2],[12], [15], [4,10], [1,9], [4,8]]
 
 
+ROWS_CAN_IMPROVE = [True for x in range(ROWS)]  # is there a difference from the previous round
+COLUMNS_CAN_IMPROVE = [True for x in range(COLUMNS)]  # --"--
+
+ITER_AMOUNT = min(5, ROWS)  # we want to take the ITER_AMOUNT best iterations
 
 
 def main():
@@ -27,9 +33,15 @@ def main():
     has_improvement = True
 
     begin = time.time()
-
-    while has_improvement:
+    while (has_improvement or (True in ROWS_CAN_IMPROVE) or (True in COLUMNS_CAN_IMPROVE)):
         has_improvement = solve_1_iteration()
+        
+        #print("has_improvement: ", has_improvement)
+        #print("True in ROWS_CAN_IMPROVE: ", (True in ROWS_CAN_IMPROVE))
+        #print("True in COLUMNS_CAN_IMPROVE: ", (True in COLUMNS_CAN_IMPROVE))
+        #print_nonogram()
+        #print("")
+        
 
     end = time.time()
 
@@ -43,19 +55,29 @@ def main():
     print("enters to rec: " + str(REC_COUNTER[0]))
     print("time took is: " + str(round(end - begin, 3)))
      
+
     
 def solve_1_iteration():
     has_improvement = False
 
-    for i in range(0, ROWS):
+    best_order_rows = get_best_order("rows")
+    best_can_impr_rows = list(filter(lambda i: ROWS_CAN_IMPROVE[i] == True, best_order_rows))  # the best rows that can be improved
+
+    for i in best_can_impr_rows[:ITER_AMOUNT]:
         tmp_has_imp = update_1_row(values_rows_arr[i], i)
         if tmp_has_imp: 
             has_improvement = True
+        #print("after " + str(i) + "'th iteration:")
+        #print_nonogram()
+        #print("rec times: " + str(REC_COUNTER))
 
     #print("after rows iteration:")
     #print_nonogram()  # to debug purposes
+    
+    best_order_columns = get_best_order("columns")
+    best_can_impr_columns = list(filter(lambda i: COLUMNS_CAN_IMPROVE[i] == True, best_order_columns))  # the best columns that can be improved
 
-    for i in range(0, COLUMNS):
+    for i in best_can_impr_columns[:ITER_AMOUNT]:
         tmp_has_imp = update_1_column(values_columns_arr[i], i)
         if tmp_has_imp: 
             has_improvement = True
@@ -76,6 +98,12 @@ def update_1_row(row_values, row_idx):
     intersection = get_intersection(valid_perms)
     Matrix[row_idx] = intersection
     
+    # update COLUMNS_CAN_IMPROVE and make false in this
+    ROWS_CAN_IMPROVE[row_idx] = False
+    for i in range(len(row_content)):
+        if (row_content[i] == state.Unknown and intersection[i] == state.Black) or (row_content[i] == state.Unknown and intersection[i] == state.White):
+            COLUMNS_CAN_IMPROVE[i] = True
+
     # return whether there was an improvement
     has_improvement = False
     if has_diff(row_content, intersection):
@@ -93,6 +121,12 @@ def update_1_column(column_values, column_idx):
     #find the 'agreed' values and update the matrix
     intersection = get_intersection(valid_perms)
     enter_column_content(intersection, column_idx)
+
+    # update ROWS_CAN_IMPROVE and make false in this
+    COLUMNS_CAN_IMPROVE[column_idx] = False
+    for i in range(len(column_content)):
+        if (column_content[i] == state.Unknown and intersection[i] == state.Black) or (column_content[i] == state.Unknown and intersection[i] == state.White):
+            ROWS_CAN_IMPROVE[i] = True
 
     # return whether there was an improvement
     has_improvement = False
@@ -116,12 +150,12 @@ def rec_get_perms(meanwhile_content, remaining_row, con_filter):
         
         first_beginning_index = find_first_unknown(meanwhile_content)
         min_place_for_remaining_row = max(sum(map(lambda x: x+1, remaining_row[1:])) - 1, 0)  # add 1 to each value for the space between each two. then sub 1 to the last one without space
-        last_beginning_index = len(meanwhile_content) - curr_to_add + 1 - min_place_for_remaining_row
+        last_beginning_index = len(meanwhile_content) - curr_to_add + 1 - min_place_for_remaining_row # TODO: should subtract 1 because indexing starts from 0?
         
         for i in range(first_beginning_index, last_beginning_index):
             new_content = fill_curr_value(meanwhile_content, i, curr_to_add)
             if con_filter(new_content):  # the new content doesn't contradict the already-existant content
-                perms += rec_get_perms(new_content, remaining_row[1:], con_filter)
+                perms += list(filter(con_filter, rec_get_perms(new_content, remaining_row[1:], con_filter)))
         return perms
 
 
@@ -140,6 +174,23 @@ def fill_curr_value(prev_content, first_index, curr_to_add):
 def enter_column_content(intersection, column_idx):
     for i in range(len(intersection)):
         Matrix[i][column_idx] = intersection[i]
+
+
+def get_best_order(direction):
+    if direction == 'rows':
+        to_sort = values_rows_arr[:]
+    else:
+        to_sort = values_columns_arr[:]
+    priority = [[i,0] for i in range(len(to_sort))]
+    
+    for i in range(len(to_sort)):  # filling priorities, rules of thumb
+       priority[i][1] += max(to_sort[i]) * 2  # it's good to have a big number
+       priority[i][1] -= COLUMNS - (sum(to_sort[i]) + len(to_sort[i]) - 1)  # if the values are very close to the line size - it's better
+       priority[i][1] -= len(to_sort[i])  # better computentially to have few values
+       # TODO: take in account how much content of line has values different from Unknown - how much it is full?
+       # TODO: prioritize lines that are in the edges
+
+    return list(map(lambda x: x[0], sorted(priority, key = lambda x: x[1], reverse = True)))
 
 
 def get_filter(line_content):
@@ -213,6 +264,29 @@ if __name__ == "__main__":
 # TODO: using pyInstaller to convert to .exe
 # The main issue to convert it to a single file
 """
+# some basic shape
+
+ROWS = 6
+COLUMNS = 6
+Matrix = [[state.Unknown for x in range(COLUMNS)] for y in range(ROWS)]
+
+#array that describes the rows of the matrix, up to down
+values_rows_arr = [[2,1],[1,3],[1,2],[3],[4],[1]]
+#array that describes the columns of the matrix, left to right
+values_columns_arr = [[1],[5],[2],[5],[2,1],[2]]
+
+
+# fish?
+
+ROWS = 10
+COLUMNS = 10
+Matrix = [[state.Unknown for x in range(COLUMNS)] for y in range(ROWS)]
+
+#array that describes the rows of the matrix, up to down
+values_rows_arr = [[1,1], [2,2], [1,6], [1,3,1], [1,4,1],[1,7], [8], [4,1], [3,2], [4]]
+#array that describes the columns of the matrix, left to right
+values_columns_arr = [[3], [3], [2], [5], [8],[6,2], [7,1], [1,2,1], [2,2,1], [6,2]]
+
 
 # this example creates אייל
 
